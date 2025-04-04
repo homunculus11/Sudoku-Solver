4e0b937c9a514a6da3eee2bd16a04db03d5ef390
homunculus11 <macarinicolae2@gmail.com>
2025-04-04 22:35:18 +0300

    Memory Use Upgrade
    Changed the 8 if-elses into one big line of code if. Making the code more optimized in memory use. Final version of this leetcode problem and of this function, maybe I will make some changes only to make it accessible to other files.

--- valid-sudoku-function.cpp
+++ valid-sudoku-function.cpp
@@ -1,3 +1,4 @@
+//Leetcode Final Solution for Valid Sudoku in C++
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -11,24 +12,9 @@ public:
                     }
 
     for (int i=0; i<7; i+=3)          //goes from square to square 3x3
-        for(int j=0; j<7; j+=3){      //goes from square to square 3x3
-            if( (board[i][j]!='.') && (board[i][j]==board[i][j+1] || board[i][j]==board[i][j+2] || board[i][j]==board[i+1][j] || board[i][j]==board[i+1][j+1] || board[i][j]==board[i+1][j+2] || board[i][j]==board[i+1][j+2] || board[i][j]==board[i+2][j] || board[i][j]==board[i+2][j+1] || board[i][j]==board[i+2][j+2]) )
+        for(int j=0; j<7; j+=3)      //goes from square to square 3x3
+            if( ((board[i][j]!='.') && (board[i][j]==board[i][j+1] || board[i][j]==board[i][j+2] || board[i][j]==board[i+1][j] || board[i][j]==board[i+1][j+1] || board[i][j]==board[i+1][j+2] || board[i][j]==board[i+1][j+2] || board[i][j]==board[i+2][j] || board[i][j]==board[i+2][j+1] || board[i][j]==board[i+2][j+2])) || ((board[i][j+1]!='.') && (board[i][j+1]==board[i][j+2] || board[i][j+1]==board[i+1][j] || board[i][j+1]==board[i+1][j+1] || board[i][j+1]==board[i+1][j+2] || board[i][j+1]==board[i+1][j+2] || board[i][j+1]==board[i+2][j] || board[i][j+1]==board[i+2][j+1] || board[i][j+1]==board[i+2][j+2])) || ((board[i][j+2]!='.') && (board[i][j+2]==board[i+1][j] || board[i][j+2]==board[i+1][j+1] || board[i][j+2]==board[i+1][j+2] || board[i][j+2]==board[i+1][j+2] || board[i][j+2]==board[i+2][j] || board[i][j+2]==board[i+2][j+1] || board[i][j+2]==board[i+2][j+2])) || ((board[i+1][j]!='.') && (board[i+1][j]==board[i+1][j+1] || board[i+1][j]==board[i+1][j+2] || board[i+1][j]==board[i+1][j+2] || board[i+1][j]==board[i+2][j] || board[i+1][j]==board[i+2][j+1] || board[i+1][j]==board[i+2][j+2])) || ((board[i+1][j+1]!='.') && (board[i+1][j+1]==board[i+1][j+2] || board[i+1][j+1]==board[i+2][j] || board[i+1][j+1]==board[i+2][j+1] || board[i+1][j+1]==board[i+2][j+2])) || ((board[i+1][j+2]!='.') && (board[i+1][j+2]==board[i+2][j] || board[i+1][j+2]==board[i+2][j+1] || board[i+1][j+2]==board[i+2][j+2])) || ((board[i+2][j]!='.') && (board[i+2][j]==board[i+2][j+1] || board[i+2][j]==board[i+2][j+2])) || (board[i+2][j+1]!='.' && board[i+2][j+1]==board[i+2][j+2]) )
                 return false;
-            else if ( (board[i][j+1]!='.') && (board[i][j+1]==board[i][j+2] || board[i][j+1]==board[i+1][j] || board[i][j+1]==board[i+1][j+1] || board[i][j+1]==board[i+1][j+2] || board[i][j+1]==board[i+1][j+2] || board[i][j+1]==board[i+2][j] || board[i][j+1]==board[i+2][j+1] || board[i][j+1]==board[i+2][j+2]) )
-                return false;
-            else if ( (board[i][j+2]!='.') && (board[i][j+2]==board[i+1][j] || board[i][j+2]==board[i+1][j+1] || board[i][j+2]==board[i+1][j+2] || board[i][j+2]==board[i+1][j+2] || board[i][j+2]==board[i+2][j] || board[i][j+2]==board[i+2][j+1] || board[i][j+2]==board[i+2][j+2]) )
-                return false;
-            else if ( (board[i+1][j]!='.') && (board[i+1][j]==board[i+1][j+1] || board[i+1][j]==board[i+1][j+2] || board[i+1][j]==board[i+1][j+2] || board[i+1][j]==board[i+2][j] || board[i+1][j]==board[i+2][j+1] || board[i+1][j]==board[i+2][j+2]) )
-                return false;
-            else if ( (board[i+1][j+1]!='.') && (board[i+1][j+1]==board[i+1][j+2] || board[i+1][j+1]==board[i+2][j] || board[i+1][j+1]==board[i+2][j+1] || board[i+1][j+1]==board[i+2][j+2]) )
-                return false;
-            else if ( (board[i+1][j+2]!='.') && (board[i+1][j+2]==board[i+2][j] || board[i+1][j+2]==board[i+2][j+1] || board[i+1][j+2]==board[i+2][j+2]) )
-                return false;
-            else if ( (board[i+2][j]!='.') && (board[i+2][j]==board[i+2][j+1] || board[i+2][j]==board[i+2][j+2]) )
-                return false;
-            else if (board[i+2][j+1]!='.' && board[i+2][j+1]==board[i+2][j+2])
-                return false;
-        }
     
     return true;
 
