4ec1c49dbec223831c77c5ae15726acad936de40
homunculus11 <macarinicolae2@gmail.com>
2025-04-04 22:45:56 +0300

    Add files via upload

---
+++ valid-sudoku-function.js
@@ -0,0 +1,18 @@
+var isValidSudoku = function(board) {
+
+    // O(n3) approach for horizontal and vertical lines check in board
+    for(let i=0; i<9; i++)
+        for(let j=0; j<8; j++)
+            for(let k=j+1; k<9; k++)
+                if( (board[i][j] === board[i][k] && board[i][j]!=='.') || (board[j][i] === board[k][i] && board[j][i]!=='.') )
+                    return false;
+                    
+
+    for (let i=0; i<7; i+=3)          //goes from square to square 3x3
+        for(let j=0; j<7; j+=3)      //goes from square to square 3x3
+            if( ((board[i][j]!=='.') && (board[i][j]===board[i][j+1] || board[i][j]===board[i][j+2] || board[i][j]===board[i+1][j] || board[i][j]===board[i+1][j+1] || board[i][j]===board[i+1][j+2] || board[i][j]===board[i+1][j+2] || board[i][j]===board[i+2][j] || board[i][j]===board[i+2][j+1] || board[i][j]===board[i+2][j+2])) || ((board[i][j+1]!=='.') && (board[i][j+1]===board[i][j+2] || board[i][j+1]===board[i+1][j] || board[i][j+1]===board[i+1][j+1] || board[i][j+1]===board[i+1][j+2] || board[i][j+1]===board[i+1][j+2] || board[i][j+1]===board[i+2][j] || board[i][j+1]===board[i+2][j+1] || board[i][j+1]===board[i+2][j+2])) || ((board[i][j+2]!=='.') && (board[i][j+2]===board[i+1][j] || board[i][j+2]===board[i+1][j+1] || board[i][j+2]===board[i+1][j+2] || board[i][j+2]===board[i+1][j+2] || board[i][j+2]===board[i+2][j] || board[i][j+2]===board[i+2][j+1] || board[i][j+2]===board[i+2][j+2])) || ((board[i+1][j]!='.') && (board[i+1][j]==board[i+1][j+1] || board[i+1][j]==board[i+1][j+2] || board[i+1][j]==board[i+1][j+2] || board[i+1][j]===board[i+2][j] || board[i+1][j]===board[i+2][j+1] || board[i+1][j]===board[i+2][j+2])) || ((board[i+1][j+1]!=='.') && (board[i+1][j+1]===board[i+1][j+2] || board[i+1][j+1]===board[i+2][j] || board[i+1][j+1]===board[i+2][j+1] || board[i+1][j+1]===board[i+2][j+2])) || ((board[i+1][j+2]!=='.') && (board[i+1][j+2]===board[i+2][j] || board[i+1][j+2]===board[i+2][j+1] || board[i+1][j+2]===board[i+2][j+2])) || ((board[i+2][j]!=='.') && (board[i+2][j]===board[i+2][j+1] || board[i+2][j]===board[i+2][j+2])) || (board[i+2][j+1]!=='.' && board[i+2][j+1]===board[i+2][j+2]) )
+                return false;
+    
+    return true;
+
+};
